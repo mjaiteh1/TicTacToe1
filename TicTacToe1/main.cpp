a300481dfa7d7b42665bf5a7a165ca5d6d4d8aad
Mariama Jaiteh <mjaiteh@smith.edu>
2016-10-06 16:04:04 -0400

    Initial Commit

---
+++ TicTacToe1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe1
+//
+//  Created by Mariama Jaiteh on 10/6/16.
+//  Copyright © 2016 Mariama Jaiteh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
